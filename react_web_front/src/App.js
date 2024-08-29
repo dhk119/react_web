82ebf064d72e87b9d455d7818c3063e3675dae65
dhk119 <daehean95@gmail.com>
2024-08-29 17:59:40 +0900

    header footer 메인화면 작성

--- react_web_front/src/App.js
+++ react_web_front/src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
+import Footer from "./components/Footer";
+import Main from "./components/common/Main";
 
 function App() {
   return (
     <div className="wrap">
       <Header />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
